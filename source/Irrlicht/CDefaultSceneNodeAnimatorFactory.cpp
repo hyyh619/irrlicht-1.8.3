44cb21c180e19b43ca709f2803e2b56489665c5a
Huang Ying <hyyh619@hotmail.com>
2024-02-05 10:51:05 +0800

    beautify code

--- source/Irrlicht/CDefaultSceneNodeAnimatorFactory.cpp
+++ source/Irrlicht/CDefaultSceneNodeAnimatorFactory.cpp
@@ -11,152 +11,157 @@
 
 namespace irr
 {
-namespace scene
-{
+    namespace scene
+    {
+// ! Names for scene node types
+        const c8* const    SceneNodeAnimatorTypeNames[] =
+        {
+            "flyCircle",
+            "flyStraight",
+            "followSpline",
+            "rotation",
+            "texture",
+            "deletion",
+            "collisionResponse",
+            "cameraFPS",
+            "cameraMaya",
+            0
+        };
 
-//! Names for scene node types
-const c8* const SceneNodeAnimatorTypeNames[] =
-{
-    "flyCircle",
-    "flyStraight",
-    "followSpline",
-    "rotation",
-    "texture",
-    "deletion",
-    "collisionResponse",
-    "cameraFPS",
-    "cameraMaya",
-    0
-};
 
-
-CDefaultSceneNodeAnimatorFactory::CDefaultSceneNodeAnimatorFactory(ISceneManager* mgr, gui::ICursorControl* crs)
-: Manager(mgr), CursorControl(crs)
-{
+        CDefaultSceneNodeAnimatorFactory::CDefaultSceneNodeAnimatorFactory(ISceneManager *mgr, gui::ICursorControl *crs)
+            : Manager(mgr), CursorControl(crs)
+        {
     #ifdef _DEBUG
-    setDebugName("CDefaultSceneNodeAnimatorFactory");
+            setDebugName("CDefaultSceneNodeAnimatorFactory");
     #endif
 
-    // don't grab the scene manager here to prevent cyclic references
-    if (CursorControl)
-        CursorControl->grab();
-}
-
-
-CDefaultSceneNodeAnimatorFactory::~CDefaultSceneNodeAnimatorFactory()
-{
-    if (CursorControl)
-        CursorControl->drop();
-}
-
-
-//! creates a scene node animator based on its type id
-ISceneNodeAnimator* CDefaultSceneNodeAnimatorFactory::createSceneNodeAnimator(ESCENE_NODE_ANIMATOR_TYPE type, ISceneNode* target)
-{
-    scene::ISceneNodeAnimator* anim = 0;
-
-    switch(type)
-    {
-    case ESNAT_FLY_CIRCLE:
-        anim = Manager->createFlyCircleAnimator(core::vector3df(0,0,0), 10);
-        break;
-    case ESNAT_FLY_STRAIGHT:
-        anim = Manager->createFlyStraightAnimator(core::vector3df(0,0,0), core::vector3df(100,100,100), 10000, true );
-        break;
-    case ESNAT_FOLLOW_SPLINE:
-        {
-            core::array<core::vector3df> points;
-            points.push_back(core::vector3df(0,0,0));
-            points.push_back(core::vector3df(10,5,10));
-            anim = Manager->createFollowSplineAnimator(0, points);
+            // don't grab the scene manager here to prevent cyclic references
+            if (CursorControl)
+                CursorControl->grab();
         }
-        break;
-    case ESNAT_ROTATION:
-        anim = Manager->createRotationAnimator(core::vector3df(0.3f,0,0));
-        break;
-    case ESNAT_TEXTURE:
+
+
+        CDefaultSceneNodeAnimatorFactory::~CDefaultSceneNodeAnimatorFactory()
         {
-            core::array<video::ITexture*> textures;
-            anim = Manager->createTextureAnimator(textures, 250);
+            if (CursorControl)
+                CursorControl->drop();
         }
-        break;
-    case ESNAT_DELETION:
-        anim = Manager->createDeleteAnimator(5000);
-        break;
-    case ESNAT_COLLISION_RESPONSE:
-        anim = Manager->createCollisionResponseAnimator(0, target);
-        break;
-    case ESNAT_CAMERA_FPS:
-        anim = new CSceneNodeAnimatorCameraFPS(CursorControl);
-        break;
-    case ESNAT_CAMERA_MAYA:
-        anim = new CSceneNodeAnimatorCameraMaya(CursorControl);
-        break;
-    default:
-        break;
-    }
-
-    if (anim && target)
-        target->addAnimator(anim);
-
-    return anim;
-}
 
 
-//! creates a scene node animator based on its type name
-ISceneNodeAnimator* CDefaultSceneNodeAnimatorFactory::createSceneNodeAnimator(const c8* typeName, ISceneNode* target)
-{
-    return createSceneNodeAnimator( getTypeFromName(typeName), target );
-}
+// ! creates a scene node animator based on its type id
+        ISceneNodeAnimator* CDefaultSceneNodeAnimatorFactory::createSceneNodeAnimator(ESCENE_NODE_ANIMATOR_TYPE type, ISceneNode *target)
+        {
+            scene::ISceneNodeAnimator    *anim = 0;
+
+            switch (type)
+            {
+                case ESNAT_FLY_CIRCLE:
+                    anim = Manager->createFlyCircleAnimator(core::vector3df(0, 0, 0), 10);
+                    break;
+
+                case ESNAT_FLY_STRAIGHT:
+                    anim = Manager->createFlyStraightAnimator(core::vector3df(0, 0, 0), core::vector3df(100, 100, 100), 10000, true);
+                    break;
+
+                case ESNAT_FOLLOW_SPLINE:
+                {
+                    core::array<core::vector3df>    points;
+                    points.push_back(core::vector3df(0, 0, 0));
+                    points.push_back(core::vector3df(10, 5, 10));
+                    anim = Manager->createFollowSplineAnimator(0, points);
+                }
+                break;
+
+                case ESNAT_ROTATION:
+                    anim = Manager->createRotationAnimator(core::vector3df(0.3f, 0, 0));
+                    break;
+
+                case ESNAT_TEXTURE:
+                {
+                    core::array<video::ITexture*>    textures;
+                    anim = Manager->createTextureAnimator(textures, 250);
+                }
+                break;
+
+                case ESNAT_DELETION:
+                    anim = Manager->createDeleteAnimator(5000);
+                    break;
+
+                case ESNAT_COLLISION_RESPONSE:
+                    anim = Manager->createCollisionResponseAnimator(0, target);
+                    break;
+
+                case ESNAT_CAMERA_FPS:
+                    anim = new CSceneNodeAnimatorCameraFPS(CursorControl);
+                    break;
+
+                case ESNAT_CAMERA_MAYA:
+                    anim = new CSceneNodeAnimatorCameraMaya(CursorControl);
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (anim && target)
+                target->addAnimator(anim);
+
+            return anim;
+        }
 
 
-//! returns amount of scene node animator types this factory is able to create
-u32 CDefaultSceneNodeAnimatorFactory::getCreatableSceneNodeAnimatorTypeCount() const
-{
-    return ESNAT_COUNT;
-}
+// ! creates a scene node animator based on its type name
+        ISceneNodeAnimator* CDefaultSceneNodeAnimatorFactory::createSceneNodeAnimator(const c8 *typeName, ISceneNode *target)
+        {
+            return createSceneNodeAnimator(getTypeFromName(typeName), target);
+        }
 
 
-//! returns type of a createable scene node animator type
-ESCENE_NODE_ANIMATOR_TYPE CDefaultSceneNodeAnimatorFactory::getCreateableSceneNodeAnimatorType(u32 idx) const
-{
-    if (idx<ESNAT_COUNT)
-        return (ESCENE_NODE_ANIMATOR_TYPE)idx;
-    else
-        return ESNAT_UNKNOWN;
-}
+// ! returns amount of scene node animator types this factory is able to create
+        u32 CDefaultSceneNodeAnimatorFactory::getCreatableSceneNodeAnimatorTypeCount() const
+        {
+            return ESNAT_COUNT;
+        }
 
 
-//! returns type name of a createable scene node animator type 
-const c8* CDefaultSceneNodeAnimatorFactory::getCreateableSceneNodeAnimatorTypeName(u32 idx) const
-{
-    if (idx<ESNAT_COUNT)
-        return SceneNodeAnimatorTypeNames[idx];
-    else
-        return 0;
-}
-
-//! returns type name of a createable scene node animator type 
-const c8* CDefaultSceneNodeAnimatorFactory::getCreateableSceneNodeAnimatorTypeName(ESCENE_NODE_ANIMATOR_TYPE type) const
-{
-    // for this factory: index == type
-
-    if (type<ESNAT_COUNT)
-        return SceneNodeAnimatorTypeNames[type];
-    else
-        return 0;
-}
-
-ESCENE_NODE_ANIMATOR_TYPE CDefaultSceneNodeAnimatorFactory::getTypeFromName(const c8* name) const
-{
-    for ( u32 i=0; SceneNodeAnimatorTypeNames[i]; ++i)
-        if (!strcmp(name, SceneNodeAnimatorTypeNames[i]) )
-            return (ESCENE_NODE_ANIMATOR_TYPE)i;
-
-    return ESNAT_UNKNOWN;
-}
+// ! returns type of a createable scene node animator type
+        ESCENE_NODE_ANIMATOR_TYPE CDefaultSceneNodeAnimatorFactory::getCreateableSceneNodeAnimatorType(u32 idx) const
+        {
+            if (idx<ESNAT_COUNT)
+                return (ESCENE_NODE_ANIMATOR_TYPE)idx;
+            else
+                return ESNAT_UNKNOWN;
+        }
 
 
-} // end namespace scene
+// ! returns type name of a createable scene node animator type
+        const c8* CDefaultSceneNodeAnimatorFactory::getCreateableSceneNodeAnimatorTypeName(u32 idx) const
+        {
+            if (idx<ESNAT_COUNT)
+                return SceneNodeAnimatorTypeNames[idx];
+            else
+                return 0;
+        }
+
+// ! returns type name of a createable scene node animator type
+        const c8* CDefaultSceneNodeAnimatorFactory::getCreateableSceneNodeAnimatorTypeName(ESCENE_NODE_ANIMATOR_TYPE type) const
+        {
+            // for this factory: index == type
+
+            if (type<ESNAT_COUNT)
+                return SceneNodeAnimatorTypeNames[type];
+            else
+                return 0;
+        }
+
+        ESCENE_NODE_ANIMATOR_TYPE CDefaultSceneNodeAnimatorFactory::getTypeFromName(const c8 *name) const
+        {
+            for (u32 i= 0; SceneNodeAnimatorTypeNames[i]; ++i)
+                if (!strcmp(name, SceneNodeAnimatorTypeNames[i]))
+                    return (ESCENE_NODE_ANIMATOR_TYPE)i;
+
+            return ESNAT_UNKNOWN;
+        }
+    } // end namespace scene
 } // end namespace irr
-
--- source/Irrlicht/CDefaultSceneNodeAnimatorFactory.h
+++ source/Irrlicht/CDefaultSceneNodeAnimatorFactory.h
@@ -9,67 +9,64 @@
 
 namespace irr
 {
-namespace gui
-{
-    class ICursorControl;
-}
-namespace scene
-{
-    class ISceneNodeAnimator;
-    class ISceneManager;
-    
-    //!    Interface making it possible to dynamicly create scene nodes animators 
-    class CDefaultSceneNodeAnimatorFactory : public ISceneNodeAnimatorFactory
+    namespace gui
     {
-    public:
+        class ICursorControl;
+    }
+    namespace scene
+    {
+        class ISceneNodeAnimator;
+        class ISceneManager;
 
-        CDefaultSceneNodeAnimatorFactory(ISceneManager* mgr, gui::ICursorControl* crs);
+        // !    Interface making it possible to dynamicly create scene nodes animators
+        class CDefaultSceneNodeAnimatorFactory : public ISceneNodeAnimatorFactory
+        {
+public:
 
-        virtual ~CDefaultSceneNodeAnimatorFactory();
+            CDefaultSceneNodeAnimatorFactory(ISceneManager *mgr, gui::ICursorControl *crs);
 
-        //! creates a scene node animator based on its type id
-        /** \param type: Type of the scene node animator to add.
-        \param target: Target scene node of the new animator.
-        \return Returns pointer to the new scene node animator or null if not successful. You need to
-        drop this pointer after calling this, see IReferenceCounted::drop() for details. */
-        virtual ISceneNodeAnimator* createSceneNodeAnimator(ESCENE_NODE_ANIMATOR_TYPE type, ISceneNode* target);
+            virtual ~CDefaultSceneNodeAnimatorFactory();
 
-        //! creates a scene node animator based on its type name
-        /** \param typeName: Type of the scene node animator to add.
-        \param target: Target scene node of the new animator.
-        \return Returns pointer to the new scene node animator or null if not successful. You need to
-        drop this pointer after calling this, see IReferenceCounted::drop() for details. */
-        virtual ISceneNodeAnimator* createSceneNodeAnimator(const char* typeName, ISceneNode* target);
+            // ! creates a scene node animator based on its type id
+            /** \param type: Type of the scene node animator to add.
+               \param target: Target scene node of the new animator.
+               \return Returns pointer to the new scene node animator or null if not successful. You need to
+               drop this pointer after calling this, see IReferenceCounted::drop() for details. */
+            virtual ISceneNodeAnimator* createSceneNodeAnimator(ESCENE_NODE_ANIMATOR_TYPE type, ISceneNode *target);
 
-        //! returns amount of scene node animator types this factory is able to create
-        virtual u32 getCreatableSceneNodeAnimatorTypeCount() const;
+            // ! creates a scene node animator based on its type name
+            /** \param typeName: Type of the scene node animator to add.
+               \param target: Target scene node of the new animator.
+               \return Returns pointer to the new scene node animator or null if not successful. You need to
+               drop this pointer after calling this, see IReferenceCounted::drop() for details. */
+            virtual ISceneNodeAnimator* createSceneNodeAnimator(const char *typeName, ISceneNode *target);
 
-        //! returns type of a createable scene node animator type
-        /** \param idx: Index of scene node animator type in this factory. Must be a value between 0 and
-        getCreatableSceneNodeTypeCount() */
-        virtual ESCENE_NODE_ANIMATOR_TYPE getCreateableSceneNodeAnimatorType(u32 idx) const;
+            // ! returns amount of scene node animator types this factory is able to create
+            virtual u32 getCreatableSceneNodeAnimatorTypeCount() const;
 
-        //! returns type name of a createable scene node animator type 
-        /** \param idx: Index of scene node animator type in this factory. Must be a value between 0 and
-        getCreatableSceneNodeAnimatorTypeCount() */
-        virtual const c8* getCreateableSceneNodeAnimatorTypeName(u32 idx) const;
+            // ! returns type of a createable scene node animator type
+            /** \param idx: Index of scene node animator type in this factory. Must be a value between 0 and
+               getCreatableSceneNodeTypeCount() */
+            virtual ESCENE_NODE_ANIMATOR_TYPE getCreateableSceneNodeAnimatorType(u32 idx) const;
 
-        //! returns type name of a createable scene node animator type 
-        /** \param type: Type of scene node animator. 
-        \return: Returns name of scene node animator type if this factory can create the type, otherwise 0. */
-        virtual const c8* getCreateableSceneNodeAnimatorTypeName(ESCENE_NODE_ANIMATOR_TYPE type) const;
+            // ! returns type name of a createable scene node animator type
+            /** \param idx: Index of scene node animator type in this factory. Must be a value between 0 and
+               getCreatableSceneNodeAnimatorTypeCount() */
+            virtual const c8* getCreateableSceneNodeAnimatorTypeName(u32 idx) const;
 
-    private:
+            // ! returns type name of a createable scene node animator type
+            /** \param type: Type of scene node animator.
+               \return: Returns name of scene node animator type if this factory can create the type, otherwise 0. */
+            virtual const c8* getCreateableSceneNodeAnimatorTypeName(ESCENE_NODE_ANIMATOR_TYPE type) const;
 
-        ESCENE_NODE_ANIMATOR_TYPE getTypeFromName(const c8* name) const;
+private:
 
-        ISceneManager* Manager;
-        gui::ICursorControl* CursorControl;
-    };
+            ESCENE_NODE_ANIMATOR_TYPE getTypeFromName(const c8 *name) const;
 
-
-} // end namespace scene
+            ISceneManager           *Manager;
+            gui::ICursorControl     *CursorControl;
+        };
+    } // end namespace scene
 } // end namespace irr
 
 #endif
-
