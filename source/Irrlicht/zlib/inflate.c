3ce7949fe53d8d25181ce840f83d8a3cd30ed75e
Huang Ying <hyyh619@hotmail.com>
2026-03-25 08:51:10 +0800

    Fix warning in inflateMark().

--- source/Irrlicht/zlib/inflate.c
+++ source/Irrlicht/zlib/inflate.c
@@ -1453,8 +1453,11 @@ long ZEXPORT inflateMark(z_streamp strm)
 {
     struct inflate_state FAR *state;
 
-    if (strm == Z_NULL || strm->state == Z_NULL) return -1L << 16;
+    if (strm == Z_NULL || strm->state == Z_NULL)
+        return 0xffffffff << 16;
+
     state = (struct inflate_state FAR *)strm->state;
+
     return ((long)(state->back) << 16) +
         (state->mode == COPY ? state->length :
             (state->mode == MATCH ? state->was - state->length : 0));
