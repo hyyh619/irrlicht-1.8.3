abb9e3e7428cb817ae621ae33a200f42ada6aeb7
Huang Ying <hyyh619@hotmail.com>
2026-04-27 14:15:05 +0800

    Do not enable multithread building.

--- examples/build_debug_x64_conform.bat
+++ examples/build_debug_x64_conform.bat
@@ -13,7 +13,7 @@ if not exist "%MSBUILD%" (
 echo Building Irrlicht Engine (x64 Debug, CONFORM_TEST)...
 echo.
 
-"%MSBUILD%" BuildAllExamples.2019.sln /p:Configuration=Debug /p:Platform=x64 /p:DefineConstants="CONFORM_TEST=1" /m /v:minimal
+"%MSBUILD%" BuildAllExamples.2019.sln /p:Configuration=Debug /p:Platform=x64 /p:DefineConstants="CONFORM_TEST=1" /v:minimal
 
 if %ERRORLEVEL% neq 0 (
     echo.
