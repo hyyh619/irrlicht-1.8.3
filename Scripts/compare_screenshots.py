5c5b1f8f2d50786fe78759c2b15ab2725c74bab7
Huang Ying <hyyh619@hotmail.com>
2026-04-28 14:45:39 +0800

    compare_screenshots.py: add tolerance for each channel of pixels.

--- Scripts/compare_screenshots.py
+++ Scripts/compare_screenshots.py
@@ -13,8 +13,13 @@ def compare_images(img1_path, img2_path):
     pixels2 = list(img2.getdata())
 
     differences = 0
+    tolerance = 30 # pixel channel value's tolerance
     for i, (p1, p2) in enumerate(zip(pixels1, pixels2)):
-        if p1 != p2:
+        # if p1 != p2:
+        
+        if abs(p1[0] - p2[0]) >= tolerance or\
+            abs(p1[1] - p2[1]) >= tolerance or\
+            abs(p1[2] - p2[2]) >= tolerance:
             differences += 1
             if differences <= 10:  # Show first 10 differences
                 x = i % img1.size[0]
@@ -24,9 +29,10 @@ def compare_images(img1_path, img2_path):
     total_pixels = img1.size[0] * img1.size[1]
 
     # If there is less than 100 different pixels, we take it as passed.
-    tolerance = 100
-    if differences <= tolerance:
-        print(f"PASS: All {total_pixels} pixels match!")
+    pixelsPassRate = 0.9
+    tolerancePixels = int(total_pixels * (1.0 - pixelsPassRate)) 
+    if differences <= tolerancePixels:
+        print(f"Total: {total_pixels} PASS: {total_pixels-differences} pixels match!, {100*(total_pixels - differences)/total_pixels:.2f}%")
         return True
     else:
         print(f"FAIL: {differences}/{total_pixels} pixels differ ({100*differences/total_pixels:.2f}%)")
@@ -36,9 +42,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"Usage: python {sys.argv[0]} <example_name>")
         print(f"Example: python {sys.argv[0]} 01.HelloWorld")
-        sys.exit(1)
+        # sys.exit(1)
+        example_name = "02.Quake3Map"
+    else:
+        example_name = sys.argv[1]
 
-    example_name = sys.argv[1]
     screenshot = f"examples/{example_name}/screenshot.bmp"
     golden = f"examples/{example_name}/Frame1-screenshot-golden.bmp"
 
