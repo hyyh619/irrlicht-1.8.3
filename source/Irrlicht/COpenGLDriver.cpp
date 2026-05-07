60ad158dbd38a87c1f656c38a61326224e4aed31
Huang Ying <hyyh619@hotmail.com>
2026-05-07 15:32:39 +0800

    Fix build error.

--- source/Irrlicht/COpenGLDriver.cpp
+++ source/Irrlicht/COpenGLDriver.cpp
@@ -1818,7 +1818,7 @@ namespace irr
         {
             ++DrawCallCounter;
 
-            IImage    *image = createScreenShot(video::ECF_A8R8G8B8, video::ERT_FRAME_BUFFER);
+            IImage    *image = createScreenShot(ECOLOR_FORMAT::ECF_A8R8G8B8, video::ERT_FRAME_BUFFER);
             if (image)
             {
                 core::stringc    filename = "draw_call_";
